de4c68e4f131707a28e675e79528392239a1aa7b
julioleao <julio0023@live.com>
2020-09-10 17:47:26 -0300

    Corrigido index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './store';
+
 
 render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </BrowserRouter>,
 
   document.getElementById('root')
 );
