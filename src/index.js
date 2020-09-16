045b56a68a900e643021a67d2f8f64934e282227
julioleao <julio0023@live.com>
2020-09-16 17:42:08 -0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import * as serviceWorker from './serviceWorker';
 
-import store from './store';
+import store from "./store";
 import App from "./App";
 
 render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.unregister();
